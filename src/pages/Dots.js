15c7f6e0209860bea573454cf2bf4e2fab1d4c60
mac <mac@MacBook-Pro-de-mac.local>
2022-10-05 16:36:48 +0100

    add some styles

--- src/pages/Dots.js
+++ src/pages/Dots.js
@@ -1,105 +1,121 @@
 /* eslint-disable eqeqeq */
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Dots = ({state, children, handelPage}) => {
+const Dots = ({ state, children, handelPage }) => {
+  const { page, count } = state;
 
-  const {page, count} = state 
+  const [show, setShow] = useState("none");
 
-  const [show, setSecroll] = useState('none')
+  const right = count / 5;
 
-  const right = count/5
+  const lenLeft =
+    page == right - 2
+      ? right - 5
+      : page > right - 3
+      ? right - 4
+      : page > 3
+      ? page - 3
+      : 0;
 
-  const lenLeft = page == right - 2 ? right - 5 : page > right - 3 ? right - 4 : page > 3 ? page - 3 : 0
-  const lenRight = page < 3 ? right - 4 : page < right - 2 ? right - page -2 : 0 
-  
-  const pageLeft = page > 3 ? 2 : 0
-  const pageRight = page < right -2 ? right - lenRight : page - 1 
+  const lenRight =
+    page < 3 ? right - 4 : page < right - 2 ? right - page - 2 : 0;
 
-  console.log(lenLeft)
+  const pageLeft = page > 3 ? 2 : 0;
+  const pageRight = page < right - 2 ? right - lenRight : page - 1;
 
-  
-  const dropLeft = [...Array(lenLeft)].map((_,idx) => {
-    return(
-    <Link onClick={()=>handelPage(pageLeft  + idx )} to={'/'+ Number(Number(pageLeft)  + idx)} key={idx}>
-      {pageLeft  + idx }
-    </Link >
-    )
-  })
+  console.log(lenLeft);
 
-  const dropRight = [...Array(lenRight)].map((_,idx) => {
-    return(
-    <Link onClick={()=>handelPage(pageRight + idx )} to={'/'+ Number(Number(pageRight) + idx)} key={idx}>
-      {pageRight + idx }
-    </Link >
-    )
-  })
+  const handelClick = (number) => {
+    handelPage(number);
+    setShow("none");
+  };
+
+  const dropLeft = [...Array(lenLeft)].map((_, idx) => {
+    return (
+      <Link
+        onClick={() => handelClick(pageLeft + idx)}
+        to={"/" + Number(Number(pageLeft) + idx)}
+        key={idx}
+      >
+        {pageLeft + idx}
+      </Link>
+    );
+  });
+
+  const dropRight = [...Array(lenRight)].map((_, idx) => {
+    return (
+      <Link
+        onClick={() => handelClick(pageRight + idx)}
+        to={"/" + Number(Number(pageRight) + idx)}
+        key={idx}
+      >
+        {pageRight + idx}
+      </Link>
+    );
+  });
 
   return (
     <Container>
-
-      {
-        page > 3 && <span onClick={() => setSecroll(show === 'left' ? 'none' : 'left')}>
-        ...
+      {page > 3 && (
+        <span onClick={() => setShow(show === "left" ? "none" : "left")}>
+          ...
         </span>
-      }
+      )}
 
-      {
-        page > 3 && show === 'left' && <div className='left'>{dropLeft}</div>
-      }
+      {page > 3 && show === "left" && <div className="left">{dropLeft}</div>}
 
       {children}
 
-      {
-      page < 8 && <span onClick={() => setSecroll(show === 'right' ? 'none' : 'right')}>
-      ...
-      </span>
-      }
-      
-      {
-        page < 8 && show === 'right' && dropRight
-      }
-        
+      {page < 8 && (
+        <span onClick={() => setShow(show === "right" ? "none" : "right")}>
+          ...
+        </span>
+      )}
 
+      {page < 8 && show === "right" && <div className="right">{dropRight}</div>}
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.span`
-    display: inline-block;
-    position:relative;
-    font-size: 12px;
-    height: 20px;
-    span{
-      fon-size: 1rem;
-      cursor:pointer;
-    }
-  .left{
-    a{
-      background: red;
+  display: inline-block;
+  position: relative;
+  font-size: 12px;
+  height: 20px;
+  padding: 0;
+  span {
+    fon-size: 1rem;
+    cursor: pointer;
+    padding: 0.5rem;
+  }
+  .left,
+  .right {
+    a {
       border: none;
-      font-size: 10px;
+      font-size: 14px;
       z-index: 11;
+      margin: 0;
+      padding: 0.3rem;
+      :hover {
+        background: #22ff55;
+      }
     }
-    position:absolute;
-    display: flex;
-    flex-direction: column;
-    z-index:9;
-    top: -120px;
+    position: absolute;
+    bottom: 10px;
+    box-shadow: 1px 1px 5px black;
+    // border: 1px solid black;
     overflow: scroll;
     scroll-behavior: smooth;
-    height: 80px;
+    max-height: 60px;
+    width: 30px;
+    padding-left: 0.5rem;
+    background: white;
   }
-  .right{
-    display: flex;
-    flex-direction: column;
-    z-index:9;
-    bottom:120px;
-    overflow: scroll;
-    scroll-behavior: smooth;
-    height: 80px;
+  .right {
+    right: -10px;
   }
-`
+`;
 
-export default Dots
+export default Dots;
--- src/pages/Pagination.js
+++ src/pages/Pagination.js
@@ -1,81 +1,90 @@
 /* eslint-disable eqeqeq */
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom'
-import Dots from './Dots';
-import LinkRoute from './LinkRoute';
+import React from "react";
+import styled from "styled-components";
+import Dots from "./Dots";
+import LinkRoute from "./LinkRoute";
 
-const Pagination = ({state, handelPage}) => {
+const Pagination = ({ state, handelPage }) => {
+  let { page } = state;
 
-  let {page} = state;
+  let midel = [...Array(Number(page) <= 2 || Number(page) >= 9 ? 2 : 3)].map(
+    (_, idx) =>
+      Number(page) == 1 ? (
+        <LinkRoute
+          handelPage={handelPage}
+          pageNumber={Number(page) + 1 + idx}
+          page={page}
+        />
+      ) : Number(page) == 2 ? (
+        <LinkRoute
+          handelPage={handelPage}
+          pageNumber={Number(page) + idx}
+          page={page}
+        />
+      ) : Number(page) == 10 ? (
+        <LinkRoute
+          handelPage={handelPage}
+          pageNumber={Number(page) + idx - 2}
+          page={page}
+        />
+      ) : (
+        // :Number(page) == 9
+        // ? <LinkRoute handelPage={handelPage} pageNumber={Number(page) + idx - 1} page={page} />
 
-  let midel = [...Array(Number(page)<=2||Number(page)>=9?2:3)].map((_,idx) => (
-      
-    Number(page) == 1
-    ? <LinkRoute handelPage={handelPage} pageNumber={Number(page) + 1 + idx} page={page} /> 
-    
-    :Number(page) == 2
-    ? <LinkRoute handelPage={handelPage} pageNumber={Number(page) + idx}     page={page} /> 
-      
-    :Number(page) == 10
-    ? <LinkRoute handelPage={handelPage} pageNumber={Number(page) + idx - 2} page={page} />  
-    
-    :Number(page) == 9
-    ? <LinkRoute handelPage={handelPage} pageNumber={Number(page) + idx - 1} page={page} />
-
-    : <LinkRoute handelPage={handelPage} pageNumber={Number(page) + idx - 1} page={page} />
-
-    ))
-
+        <LinkRoute
+          handelPage={handelPage}
+          pageNumber={Number(page) + idx - 1}
+          page={page}
+        />
+      )
+  );
 
   return (
     <Paginations>
-
       <LinkRoute handelPage={handelPage} pageNumber={1} page={page} />
 
       <Dots state={state} handelPage={handelPage}>
-        { midel}
+        {midel}
       </Dots>
 
       <LinkRoute handelPage={handelPage} pageNumber={10} page={page} />
-
     </Paginations>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
 
 const Paginations = styled.div`
   padding: 2rem 4rem 0;
-  display:flex;
-  flex-direction:row;
-  .disabled{
+  display: flex;
+  flex-direction: row;
+  .disabled {
     cursor: not-allowed;
     background: #3f3f3f;
     color: white;
   }
-  a{
+  a {
     font-size: 1rem;
     font-weight: bold;
     color: black;
-    margin: .5rem;
+    margin: 0.5rem;
     display: inline-block;
     width: 1.2rem;
     border-radius: 2px;
     text-decoration: none;
     border: 1px solid gray;
-    padding: .3rem;
+    padding: 0.3rem;
     text-align: center;
-    height:20px;
-    :active{
+    height: 20px;
+    :active {
       background: blue;
       color: white;
     }
   }
-  .disable{
+  .disable {
     cursor: not-allowed;
     color: white;
     border: #3fde3f solid 1px;
     background: #3fde3f;
   }
-`
+`;
